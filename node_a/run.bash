cd80c11ed17193a18c523dbd0ab5b3a9125baf0a
Rouli Elizabeth Geraldine Aritonang <162035717+eligeraldine@users.noreply.github.com>
2025-12-12 22:23:20 +0700

    Add files via upload

---
+++ node_a/run.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python3 peer_node.py \
+	  --name A --listen 0.0.0.0 5001 \
+	  --peers A@192.168.122.50:5001 B@192.168.122.147:5002 D@192.168.122.106:5005 \
+	  --logger 192.168.122.248 5004 \
+	  --offset-ms 600 \
+	  --initiate-broadcast --msg "Hello from A"
+
---
+++ node_c/run.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 logger.py --bind 192.168.122.248 --port 5004 --expect 10
